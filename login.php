e99c4e241246e311aba24d92264bb12fd8ffbc67
Milena <Milena@etec.com>
2022-09-27 12:20:05 -0300

    iniciando parte de login

---
+++ login.php
@@ -0,0 +1,63 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="./style.css">
+
+    <script src="./script.js" defer></script>
+    
+    <title>Login</title>
+</head>
+<body>
+      <main>
+        <section class="login">
+            <!-- <img src="" class="login__logo"> -->
+            
+            <h1 class="login__title">Fazer Login</h1>
+
+            <label class="login__label">
+                <span>nome de usuário</span>
+                <input type="text" name="username" class="input">
+            </label>
+
+            
+            <label class="login__label">
+            <span>senha</span>
+                <input type="password" name="password" class="input">
+            </label>
+
+            <div class="login__icons">
+              <button type="button" class="icons__button">
+                <!-- <img src="" alt="facebook"> -->
+              </button>
+
+              <button type="button" class="icons__button">
+                <!-- <img src="" alt="google"> -->
+              </button>
+
+              <button type="button" class="icons__button">
+                <!-- <img src="" alt="Aplee"> -->
+              </button>
+            </div>
+
+            <label class="login__label--checkbox">
+            manter login
+            </label>
+
+
+            <!-- <button type="button" class="login__button" disabled> -->
+             <!-- <svg xmlns="http://www.w3.org/2000/svg" view="0 0 448 512"> -->
+             <!-- </svg> -->
+            <!-- </button> -->
+
+            <a href="#" class="login__link">não consegue iniciar sessão?</a> 
+            <a href="#" class="login__link">criar conta</a> 
+
+        </section>
+
+        <section class="wallpaper"></section>
+      </main>   
+
+</body>
+</html>
